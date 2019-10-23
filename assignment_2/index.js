a7b618e33f937674ad62824e4c617be8fffdeec8
Alexandru Dragomir <alexandru.dragomir422@gmail.com>
2019-10-23 19:58:17 +0300

    Assignment 2

--- assignment_2/index.js
+++ assignment_2/index.js
@@ -9,17 +9,20 @@ function initCaching() {
         home: 0,
         contact: 0,
         about: 0,
+        //pageAccesCounter function
         pageAccessCounter: function () {
             if (arguments.length == 0) {
                 cache.home++;
             } else if (typeof arguments[0] !== 'undefined') {
                 if (arguments[0].toLowerCase() == 'about') {
+
                     cache.about++;
                 } else if (arguments[0].toLowerCase() == 'contact') {
                     cache.contact++;
                 }
             }
         },
+        //getCache function
         getCache: function () {
             return cache;
         }
