819e3a362e1080af8e673d8a9919305c1e75d6e0
Alexandru Dragomir <alexandru.dragomir422@gmail.com>
2019-10-23 19:54:47 +0300

    Assignement 2

---
+++ assignment_2/index.js
@@ -0,0 +1,35 @@
+const FIRST_NAME = "Dragomir";
+const LAST_NAME = "Alexandru";
+const GRUPA = "1075";
+
+
+function initCaching() {
+
+    cache = {
+        home: 0,
+        contact: 0,
+        about: 0,
+        pageAccessCounter: function () {
+            if (arguments.length == 0) {
+                cache.home++;
+            } else if (typeof arguments[0] !== 'undefined') {
+                if (arguments[0].toLowerCase() == 'about') {
+                    cache.about++;
+                } else if (arguments[0].toLowerCase() == 'contact') {
+                    cache.contact++;
+                }
+            }
+        },
+        getCache: function () {
+            return cache;
+        }
+    }
+    return cache;
+}
+
+module.exports = {
+    FIRST_NAME,
+    LAST_NAME,
+    GRUPA,
+    initCaching
+}
